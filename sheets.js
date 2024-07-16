7f1cac90f8ec3c0e9416f289237b40d0df013b9d
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-07-16 22:05:08 +0000

    auth

--- sheets.js
+++ sheets.js
@@ -1,21 +1,24 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { JWT } from 'google-auth-library';
 
 const creds = JSON.parse(Buffer.from(process.env.GOOGLE_CREDS, 'base64').toString());
 
+const serviceAccountAuth = new JWT({
+    email: creds.client_email,
+    key: creds.private_key,
+    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+  });
 
-const doc = new GoogleSpreadsheet('1HUIruK6tVZziYXZOMDDL99HEwQh508BKpB5uZBdUtCo');
+const doc = new GoogleSpreadsheet('1HUIruK6tVZziYXZOMDDL99HEwQh508BKpB5uZBdUtCo', serviceAccountAuth);
 
 const updateGoogleSheet = async (data) => {
     try {
-        await doc.useServiceAccountAuth({
-            client_email: creds.client_email,
-            private_key: creds.private_key,
-        });
 
         await doc.loadInfo();
+        console.log(doc.title);
 
         const sheet = doc.sheetsByTitle["TunaTest"];
-
+        console.log(sheet.title);
         await sheet.addRows(data);
     } catch (error) {
         console.error('Failed to update Google Sheet:', error);
