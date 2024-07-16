10b5f7de68abf9d6b05cd6e58ef32761ed98deab
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2024-07-16 17:54:18 -0400

    Create sheets.js

---
+++ sheets.js
@@ -0,0 +1,19 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+
+const creds = process.env.GOOGLE_CREDS;
+const credsJSON = JSON.parse(Buffer.from(credsEncoded, 'base64').toString());
+
+const doc = new GoogleSpreadsheet('1HUIruK6tVZziYXZOMDDL99HEwQh508BKpB5uZBdUtCo');
+
+const updateGoogleSheet = async (data) => {
+    try {
+        await doc.useServiceAccountAuth(credsJSON);
+        await doc.loadInfo();
+        const sheet = doc.sheetsByTitle["TunaTest"];
+        await sheet.addRows(data);
+    } catch (error) {
+        console.error('Failed to update Google Sheet:', error);
+    }
+};
+
+export { updateGoogleSheet };
