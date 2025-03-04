28b266391e1fff5aac28b03acd2815aeecdf316c
AbusiveTuna <89785560+AbusiveTuna@users.noreply.github.com>
2025-03-04 11:08:52 +0000

    a

--- index.js
+++ index.js
@@ -7,23 +7,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "*", // Allows any frontend to access the backend
-    methods: "GET, POST, PUT, DELETE, OPTIONS",
-    allowedHeaders: "Content-Type, Authorization"
+    origin: ['*'],
+    optionsSuccessStatus: 200
 }));
 
-// Middleware to handle preflight requests manually (if necessary)
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200); // Respond to preflight requests immediately
-    }
-
-    next();
-});
 
 app.use(express.json())
 app.use(battleshipRoutes);
